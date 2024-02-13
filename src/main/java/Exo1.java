a2a2df7c5d89b119a291399c006d70a1d6a3d30b
WazeAbi <alexdalle12@gmail.com>
2024-02-13 23:19:23 +0100

    Build(Exo1):Modifcation et ajout Rand

--- src/main/java/Exo1.java
+++ src/main/java/Exo1.java
@@ -1,7 +1,11 @@
+import java.util.Random;
+
 public class Exo1 {
     public static void main(String[] args) {
-        int tab[]={3,2,1,2,4,5,6,7,8,10};
-        for(int i = 0 ; i <tab.length ; i++){
+       int[] tab = new int[10];
+        Random random = new Random();
+        for (int i=0; i<tab.length;i++){
+            tab[i]=random.nextInt(100);
             System.out.println(tab[i]);
         }
 
