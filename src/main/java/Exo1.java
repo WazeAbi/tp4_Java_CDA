85fa6393cdc210f643677f3482464bbf768979e7
WazeAbi <alexdalle12@gmail.com>
2024-02-13 22:14:04 +0100

    Build(Exo1):Création d'un Tab + Affichage

---
+++ src/main/java/Exo1.java
@@ -0,0 +1,10 @@
+public class Exo1 {
+    public static void main(String[] args) {
+        int tab[]={3,2,1,2,4,5,6,7,8,10};
+        for(int i = 0 ; i <tab.length ; i++){
+            System.out.println(tab[i]);
+        }
+
+    }
+
+}
